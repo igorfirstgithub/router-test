eaf7bd01f8647bc164533d2c8c41ddcaa99ba627
Igor <bigbookreader@gmail.com>
2022-07-21 10:58:11 +0300

    Added propTypes for Image

--- testrouter/src/components/Image.js
+++ testrouter/src/components/Image.js
@@ -1,33 +1,42 @@
 import React, { useContext } from "react";
 import { Context } from "../PhotoContext";
+import PropTypes from "prop-types";
 
-function Image(props) {
+function Image({ className, img }) {
   const [hovered, setHovered] = React.useState(false);
   const { toggleIsFavorite } = useContext(Context);
 
   return (
     <div
-      className={`${props.className} image-container`}
+      className={`${className} image-container`}
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
     >
-      <img
-        src={props.img.url}
-        className="image-grid"
-        alt={`#${props.img.id}`}
-      />
+      <img src={img.url} className="image-grid" alt={`#${img.id}`} />
 
-      {(hovered || props.img.isFavorite) && (
+      {(hovered || img.isFavorite) && (
         <i
-          onClick={() => toggleIsFavorite(props.img.id)}
+          onClick={() => toggleIsFavorite(img.id)}
           className="ri-heart-line favorite"
         >
-          {props.img.isFavorite ? "Heart" : "Hrt"}
+          {img.isFavorite ? "Heart" : "Hrt"}
         </i>
       )}
       {hovered && <i className="ri-add-circle-line cart">Bsk</i>}
     </div>
   );
 }
+
+//https://reactjs.org/docs/typechecking-with-proptypes.html#proptypes
+
+Image.propTypes = {
+  className: PropTypes.string,
+
+  img: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool,
+  }).isRequired,
+};
 
 export default Image;
